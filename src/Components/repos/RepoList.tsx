ca817554492d437a505178ee4e8609fab9b4054a
Turki <w.sm@windowslive.com>
2022-08-26 16:31:31 +0300

    complete project

--- src/Components/repos/RepoList.tsx
+++ src/Components/repos/RepoList.tsx
@@ -1,17 +1,15 @@
-import RepoItem from "./RepoItem"
+import RepoItem from "./RepoItem";
 function RepoList({ repos }: any) {
-    return (
-        <div className="rounded-lg shadow-lg card bg-base-100">
-            <div className="card-body">
-                <h2 className="text-3xl my-4 font-bold card-title">
-                    Repos
-                </h2>
-                {repos.map((repo: any) => (
-                    <RepoItem key={repo.id} name={repo.name} />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="rounded-lg shadow-lg card bg-base-100">
+      <div className="card-body">
+        <h2 className="text-3xl my-4 font-bold card-title">Repos</h2>
+        {repos.map((repo: any) => (
+          <RepoItem repo={repo} key={repo.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default RepoList
+export default RepoList;
